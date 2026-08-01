f80dbfc32d37b64165e54e93e98d6f6b47b3930d
eben-vranken <vranken.eben@outlook.com>
2026-08-01 03:48:33 +0200

    feat: four anchor dates printing

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,23 @@ mod calendar;
 mod timescales;
 
 fn main() {
-    println!("Hello, stars!");
+    let epoch_date =
+        calendar::CalendarDate::new(2000, 01, 01, 12, 00, 00.0).expect("Not a valid date");
+    let epoch_jd = timescales::calendar_date_to_julian_date(&epoch_date);
+    println!("{:?}", epoch_jd.value());
 
-    let date = calendar::CalendarDate::new(2026, 08, 1, 00, 00, 00.0).expect("Not a valid date");
+    let unix_epoch_date =
+        calendar::CalendarDate::new(1970, 01, 01, 00, 00, 00.0).expect("Not a valid date");
+    let unix_epoch_jd = timescales::calendar_date_to_julian_date(&unix_epoch_date);
+    println!("{:?}", unix_epoch_jd.value());
 
-    let jd = timescales::calendar_date_to_julian_date(&date);
+    let mjd_origin_date =
+        calendar::CalendarDate::new(1858, 11, 17, 00, 00, 00.0).expect("Not a valid date");
+    let mjd_origin_jd = timescales::calendar_date_to_julian_date(&mjd_origin_date);
+    println!("{:?}", mjd_origin_jd.value());
 
-    println!("{:?}", jd.value());
+    let julian_day_zero_date =
+        calendar::CalendarDate::new(-4713, 11, 24, 12, 00, 00.0).expect("Not a valid date");
+    let julian_day_zero_jd = timescales::calendar_date_to_julian_date(&julian_day_zero_date);
+    println!("{:?}", julian_day_zero_jd.value());
 }
