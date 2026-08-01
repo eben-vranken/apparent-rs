a4a29528bc97498862ba2cac3eceda4da520c93d
eben-vranken <vranken.eben@outlook.com>
2026-08-01 13:37:57 +0200

    feat: add round trip conversion for julian dates

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,10 @@ mod calendar;
 mod timescales;
 
 fn main() {
-    let epoch_date =
-        calendar::CalendarDate::new(2000, 01, 01, 12, 00, 00.0).expect("Not a valid date");
+    let epoch_date = calendar::CalendarDate::new(2024, 2, 29, 6, 0, 0.0).expect("Not a valid date");
     let epoch_jd = timescales::calendar_date_to_julian_date(&epoch_date);
-    println!("{:?}", epoch_jd.value());
 
-    let unix_epoch_date =
-        calendar::CalendarDate::new(1970, 01, 01, 00, 00, 00.0).expect("Not a valid date");
-    let unix_epoch_jd = timescales::calendar_date_to_julian_date(&unix_epoch_date);
-    println!("{:?}", unix_epoch_jd.value());
+    let round_trip = timescales::julian_date_to_calendar_date(&epoch_jd);
 
-    let mjd_origin_date =
-        calendar::CalendarDate::new(1858, 11, 17, 00, 00, 00.0).expect("Not a valid date");
-    let mjd_origin_jd = timescales::calendar_date_to_julian_date(&mjd_origin_date);
-    println!("{:?}", mjd_origin_jd.value());
-
-    let julian_day_zero_date =
-        calendar::CalendarDate::new(-4713, 11, 24, 12, 00, 00.0).expect("Not a valid date");
-    let julian_day_zero_jd = timescales::calendar_date_to_julian_date(&julian_day_zero_date);
-    println!("{:?}", julian_day_zero_jd.value());
+    println!("{:?}", round_trip)
 }
--- src/timescales.rs
+++ src/timescales.rs
@@ -1,4 +1,4 @@
-use crate::calendar::CalendarDate;
+use crate::calendar::{CalendarDate, CalendarError};
 
 #[derive(Debug)]
 pub struct JdUtc {
@@ -75,4 +75,68 @@ pub fn calendar_date_to_julian_date(date: &CalendarDate) -> JdUtc {
     let fraction = (hour * 3600.0 + minute * 60.0 + second) / 86400.0;
 
     JdUtc::new(day, fraction)
+}
+
+pub fn julian_date_to_calendar_date(date: &JdUtc) -> Result<CalendarDate, CalendarError> {
+    let v = date.value() + 0.5;
+
+    // Seperate days and time
+    let shifted_jd = JdUtc::new(v.trunc(), v.fract());
+    let z = shifted_jd.value().trunc();
+    let f = shifted_jd.value().fract();
+
+    // Century adjustment
+    let mut alpha = 0.0;
+    let mut a = 0.0;
+    let mut b = 0.0;
+
+    if z >= 2299161.0 {
+        alpha = ((z - 1867216.25) / 36524.25).floor();
+        a = z + 1.0 + alpha - (alpha / 4.0).floor();
+        b = a + 1524.0;
+    } else {
+        b = z + 1524.0;
+    }
+
+    // Find Year, Month, and day
+    let c = ((b - 122.1_f64) / 365.25).floor();
+    let d = (365.25_f64 * c).floor();
+    let e = ((b - d) / 30.6001_f64).floor();
+
+    let day = b - d - (30.6001_f64 * e).floor() + f.floor();
+
+    let mut month = 0.0;
+
+    if e < 14.0 {
+        month = e - 1.0;
+    } else {
+        month = e - 13.0;
+    }
+
+    let mut year = 0.0;
+
+    if month > 2.0 {
+        year = c - 4716.0;
+    } else {
+        year = c - 4715.0;
+    }
+
+    // Get time
+    let mut hour = f * 24.0;
+
+    let mut minute = hour.fract() * 60.0;
+    let mut second = minute.fract() * 60.0;
+
+    hour = hour.floor();
+    minute = minute.floor();
+    second = second.floor();
+
+    CalendarDate::new(
+        year as i32,
+        month as u8,
+        day as u8,
+        hour as u8,
+        minute as u8,
+        second,
+    )
 }
