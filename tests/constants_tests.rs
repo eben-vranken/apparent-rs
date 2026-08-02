570e8b1d00932867bfb96733f4147d7a8ae7f0dd
eben-vranken <vranken.eben@outlook.com>
2026-08-03 01:19:19 +0200

    tests: add tests to prove degrees constants are valid

---
+++ tests/constants_tests.rs
@@ -0,0 +1,29 @@
+use apparent_rs::constants;
+
+#[test]
+fn test_radians_per_degree() {
+    let degrees_per_turn: f64 = constants::TAU / constants::RADIANS_PER_DEGREE;
+
+    assert_eq!(degrees_per_turn, 360.0);
+}
+
+#[test]
+fn test_radians_per_hour() {
+    let hours_per_turn: f64 = constants::TAU / constants::RADIANS_PER_HOUR;
+
+    assert_eq!(hours_per_turn, 24.0);
+}
+
+#[test]
+fn test_radians_per_arcsecond() {
+    let arcseconds_per_turn: f64 = constants::TAU / constants::RADIANS_PER_ARCSECOND;
+
+    assert_eq!(arcseconds_per_turn, 1296000.0);
+}
+
+#[test]
+fn test_radians_per_arcmillisecond() {
+    let milliarcseconds_per_turn: f64 = constants::TAU / constants::RADIANS_PER_ARCMILLISECOND;
+
+    assert_eq!(milliarcseconds_per_turn, 1296000000.0);
+}
