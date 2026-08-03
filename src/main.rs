05c73bf61fdbe6bedca5a6d2f575ae201eadc825
eben-vranken <vranken.eben@outlook.com>
2026-08-03 18:19:14 +0200

    feat: add constructor for vector 3

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,7 @@ use apparent_rs::vec3::Vec3;
 fn main() {
     println!("Hello, stars!");
 
-    let vec_a: Vec3 = Vec3 {
-        x: 1.0,
-        y: 1.0,
-        z: 1.0,
-    };
-
-    let vec_b: Vec3 = Vec3 {
-        x: 1.0,
-        y: -1.0,
-        z: 1.0,
-    };
+    let vec_a: Vec3 = Vec3::new(1.0, 1.0, 1.0);
 
     println!("{:?}", -vec_a * 10.0)
 }
--- src/vec3.rs
+++ src/vec3.rs
@@ -9,6 +9,12 @@ pub struct Vec3 {
     pub z: f64,
 }
 
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self { x: x, y: y, z: z }
+    }
+}
+
 impl Add for Vec3 {
     type Output = Vec3;
 
