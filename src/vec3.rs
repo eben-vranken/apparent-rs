b7a8588778c644ec3057d530fb6ce510cad1fde5
eben-vranken <vranken.eben@outlook.com>
2026-08-03 17:36:45 +0200

    refactor: fix upper case struct name

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,5 +1,5 @@
 #[Derive(Clone, Copy, Debug, PartialEq)]
-pub struct vec3 {
+pub struct Vec3 {
     // I'm okay with these fields being public, since they don't need any validation
     // Any three numbers are a legitimate field
     pub x: f64,
