678381c9df0760e2cfe50c8a85f6990d7e597779
eben-vranken <vranken.eben@outlook.com>
2026-08-03 19:08:15 +0200

    test: add vector 3 operator tests

--- tests/vector_tests.rs
+++ tests/vector_tests.rs
@@ -60,3 +60,44 @@ fn test_normalize_general_vector_has_unit_length() {
 
     assert_close(vec, 1.0, TOLERANCE);
 }
+
+#[test]
+fn test_add() {
+    let vec_a = Vec3::new(1.0, 2.0, 3.0);
+    let vec_b = Vec3::new(4.0, 5.0, 6.0);
+    let sum_vec = vec_a + vec_b;
+
+    assert_eq!(sum_vec, Vec3::new(5.0, 7.0, 9.0));
+}
+
+#[test]
+fn test_sub() {
+    let vec_a = Vec3::new(1.0, 2.0, 3.0);
+    let vec_b = Vec3::new(4.0, 5.0, 6.0);
+    let sub_vec = vec_b - vec_a;
+
+    assert_eq!(sub_vec, Vec3::new(3.0, 3.0, 3.0));
+}
+
+#[test]
+fn test_mul_by_scalar() {
+    let vec_a = Vec3::new(1.0, 2.0, 3.0);
+    let scaled_vec = vec_a * 2.0;
+
+    assert_eq!(scaled_vec, Vec3::new(2.0, 4.0, 6.0));
+}
+
+#[test]
+fn test_mul_by_negative_scalar_reverses() {
+    let vec_a = Vec3::new(1.0, 2.0, 3.0);
+    let scaled_vec = vec_a * -1.0;
+
+    assert_eq!(scaled_vec, Vec3::new(-1.0, -2.0, -3.0));
+}
+
+#[test]
+fn test_neg() {
+    let vec_a = Vec3::new(1.0, 2.0, 3.0);
+
+    assert_eq!(-vec_a, Vec3::new(-1.0, -2.0, -3.0));
+}
