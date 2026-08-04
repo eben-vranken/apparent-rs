ece492e5aa6a898630d743105e7595f9585b5232
eben-vranken <vranken.eben@outlook.com>
2026-08-04 15:59:50 +0200

    feat: add star catalogue constructor

--- src/stars.rs
+++ src/stars.rs
@@ -1,4 +1,7 @@
+use crate::constants::{RADIANS_PER_DEGREE, RADIANS_PER_MILLIARCSECOND};
+
 // I have added comments as to what these values mean, cause I will forget.
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct CatalogueStar {
     /// ICRS right ascension at `epoch_jd_tt`
     pub ra_rad: f64,
@@ -19,4 +22,26 @@ pub struct CatalogueStar {
     pub rv_km_per_s: f64,
 
     pub epoch_jd_tt: f64,
+}
+
+impl CatalogueStar {
+    pub const fn from_catalog(
+        ra_deg: f64,
+        dec_deg: f64,
+        pm_ra_cosdec_mas_per_yr: f64,
+        pm_dec_mas_per_yr: f64,
+        parallax_mas: f64,
+        rv_km_per_s: f64,
+        epoch_jd_tt: f64,
+    ) -> Self {
+        Self {
+            ra_rad: ra_deg * RADIANS_PER_DEGREE,
+            dec_rad: dec_deg * RADIANS_PER_DEGREE,
+            pm_ra_cosdec_rad_per_yr: pm_ra_cosdec_mas_per_yr * RADIANS_PER_MILLIARCSECOND,
+            pm_dec_rad_per_yr: pm_dec_mas_per_yr * RADIANS_PER_MILLIARCSECOND,
+            parallax_rad: parallax_mas * RADIANS_PER_MILLIARCSECOND,
+            rv_km_per_s,
+            epoch_jd_tt,
+        }
+    }
 }
