b5a5c659864a5f4c10beaf96ed8dc566819a3929
eben-vranken <vranken.eben@outlook.com>
2026-08-04 03:14:52 +0200

    feat: initial identity (do nothing) const

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod angles;
 pub mod calendar;
 pub mod constants;
+pub mod mat3;
 pub mod timescales;
 pub mod vec3;
--- src/mat3.rs
+++ src/mat3.rs
@@ -1,8 +1,28 @@
-use apparent_rs::vec3::Vec3;
+use crate::vec3::Vec3;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Mat3 {
-    xAxis: Vec3,
-    yAxis: Vec3,
-    zAxis: Vec3,
+    row_x: Vec3,
+    row_y: Vec3,
+    row_z: Vec3,
+}
+
+impl Mat3 {
+    pub const IDENTITY: Mat3 = Self {
+        row_x: Vec3 {
+            x: 1.0,
+            y: 0.0,
+            z: 0.0,
+        },
+        row_y: Vec3 {
+            x: 0.0,
+            y: 1.0,
+            z: 0.0,
+        },
+        row_z: Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: 1.0,
+        },
+    };
 }
